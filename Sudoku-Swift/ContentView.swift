5b7789d88c12f6570af1b577181a945f2d26a811
Miranda Ragland <mirandajanell@gmail.com>
2023-06-16 23:07:36 -0700

    Draw the group grid.

--- Sudoku-Swift/ContentView.swift
+++ Sudoku-Swift/ContentView.swift
@@ -9,11 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        let groupBorder = 3.0
+        let bgColors = [
+            Color.red, Color.orange, Color.yellow,
+            Color.green, Color.blue, Color.purple,
+            Color.pink, Color.cyan, Color.mint
+        ]
+        
+        Grid(horizontalSpacing: groupBorder, verticalSpacing: groupBorder) {
+            ForEach(0..<3) { groupRow in
+                GridRow {
+                    ForEach(0..<3) { groupCol in
+                        Rectangle()
+                            .frame(width:100, height: 100)
+                            .foregroundColor(bgColors[groupRow * 3 + groupCol])
+                    }
+                }
+            }
         }
         .padding()
     }
