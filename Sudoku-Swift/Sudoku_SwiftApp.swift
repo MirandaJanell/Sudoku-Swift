f97a3174461c4dbd01b2f3d16378347f11eee9aa
Miranda Ragland <mirandajanell@gmail.com>
2023-06-16 23:03:14 -0700

    Initial Commit

---
+++ Sudoku-Swift/Sudoku_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sudoku_SwiftApp.swift
+//  Sudoku-Swift
+//
+//  Created by Miranda Ragland on 2023-06-16.
+//
+
+import SwiftUI
+
+@main
+struct Sudoku_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
